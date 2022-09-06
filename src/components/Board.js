8647020694664d406196d212bad4d70bc44a2e6b
JessAthey <jelaineya@gmail.com>
2022-09-06 12:47:04 -0400

    advanced day one

---
+++ src/components/Board.js
@@ -0,0 +1,15 @@
+import '../components/Board.js';
+
+function Board(){
+    return (
+    <div>
+        <div className="div-board">
+            <div className="row">
+            </div>
+        <button className="btn btn-success add-button">Add</button>
+        </div>
+    </div>
+    );
+}
+
+export default Board;
